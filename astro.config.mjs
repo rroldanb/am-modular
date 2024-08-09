1b5f412fc4de8840e1acdd613c3e831b7060ba61
Ruben Roldan <ruben.roldan.b@gmail.com>
2024-08-09 19:22:52 -0400

    acordiones y video hd

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+
+import icon from "astro-icon";
+
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()
+    , icon()
+  ]
 });
